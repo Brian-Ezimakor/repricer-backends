42d3920283cec46bbcccb554ef2ac06ed614be23
Brian Ezimakor <iheayichukwuezimako@gmail.com>
2026-02-20 09:12:39 +0200

    Fixed app.js for / error

--- src/app.js
+++ src/app.js
@@ -2,20 +2,26 @@ const express = require("express");
 const { handleTakealotWebhook } = require("./modules/webhooks/takealotWebhook");
 
 const app = express();
-// app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
-});
-
-app.post("/webhooks/takealot", handleTakealotWebhook);
-
-// 🔥 products API
 app.use(
   "/webhooks/takealot",
   express.raw({ type: "application/json" })
 );
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    service: "Repricer Backend",
+    timestamp: new Date(),
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.post("/webhooks/takealot", handleTakealotWebhook);
 
 module.exports = app;
